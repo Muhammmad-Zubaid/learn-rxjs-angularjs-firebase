d87af92d56cdff9155119978a7151a8cb8ecb9cb
Zia Khan <ziaukhan@hotmail.com>
2016-03-16 16:31:37 +0500

    step 3 completed

---
+++ step03_sync/app/main.js
@@ -0,0 +1,87 @@
+;(function() {
+  angular.module('example', ['rx']) .controller('AppCtrl', function($scope, firebaseRef) {
+    var usersRef = firebaseRef.child("users");
+    usersRef.update({"zia": {
+        name: "Zia Khan",
+        location: "Karachi"
+    }});
+
+    $scope.users = [];
+    var source = usersRef.observe('child_added');
+    console.log(source instanceof Rx.Observable);
+    source.subscribe(function(changeData) {
+        // If event type is 'value', changeData is a DataSnapshot
+        // Otherwise, changeData is {snapshot: DataSnapshot, prevName: optional string of previous child location}
+        var user = {
+            id : changeData.snapshot.key(),
+            data: changeData.snapshot.val()
+        };
+        
+        console.log( user.id + ": " + JSON.stringify(user.data));
+        $scope.users.push(user);
+        //$scope.$apply();
+    });
+    
+    var source2 = usersRef.observe("child_removed");//it will be called when u delete a user from the firebase directly
+    source2.subscribe(function(changeData){
+        console.log("child removed called");
+        var id = changeData.snapshot.key();
+        $scope.users.forEach(function(v, i){
+            if(v.id == id){
+                $scope.users.remove(i);
+                $scope.$apply();
+            }
+            
+        })
+    });
+   
+   
+
+    usersRef.update({"zeeshan": {
+        name: "Zeeshan Hanif",
+        location: "USA"
+    }});
+    
+  }).factory("firebaseRef", function(rx){
+   //https://gist.github.com/gsoltis/ee20138502a4764650f2
+   
+   var makeCallback = function(eventType, observer) {
+    if (eventType === 'value') {
+      return function(snap) {
+        observer.onNext(snap);
+      };
+    } else {
+      return function(snap, prevName) {
+        // Wrap into an object, since we can only pass one argument through.
+        observer.onNext({snapshot: snap, prevName: prevName});
+      }
+    }
+  };
+  
+  Firebase.prototype.__proto__.observe = function(eventType) {
+    var query = this;
+    return rx.Observable.create(function(observer) {
+      var listener = query.on(eventType, makeCallback(eventType, observer), function(error) {
+        observer.onError(error);
+      });
+      return function() {
+        query.off(eventType, listener);
+      }
+    }).publish().refCount();
+  };
+   
+  var myFirebaseRef = new Firebase("https://multi-test.firebaseio.com/"); 
+      
+  return myFirebaseRef;
+      
+      
+  });
+}());
+
+
+// Array Remove - By John Resig (MIT Licensed) http://stackoverflow.com/questions/500606/javascript-array-delete-elements
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
---
+++ step03_sync/readme.md
@@ -0,0 +1,7 @@
+Using AngularJS 1.5 with Rxjs and Firebase:
+
+https://github.com/Reactive-Extensions/rx.angular.js
+
+https://gist.github.com/gsoltis/ee20138502a4764650f2
+
+
